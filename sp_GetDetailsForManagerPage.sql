e2238c3a60341705b852451d124f77bd6b88f52e
Janani Somasundaram <JananiS@softura.com>
2022-10-11 19:50:27 +0530

    first commit

---
+++ sp_GetDetailsForManagerPage.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE sp_GetDetailsForManager
+AS
+BEGIN
+	SELECT 
+		emp.EmpId,
+		emp.EmpName,
+		sk.SkillName,
+		emp.Profile,
+		emp.Experience,
+		emp.ManagerName,
+		sftlck.IsLocked,
+		sftlck.LockId
+	FROM Employee emp
+	 JOIN SkillMap skm ON skm.EmpId  = emp.EmpId 
+	 JOIN Skill sk ON skm.SkillId = sk.SkillId 
+	 JOIN SoftLock sftlck ON sftlck.LockId = emp.LockId
+	where sftlck.IsLocked = 1
+END
